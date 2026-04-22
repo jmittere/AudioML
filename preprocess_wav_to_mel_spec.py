adding normalization by frequency bin across dataset and fixed waveform generation

preprocess_wav_to_mel_spec.py
@@ -9,6 +9,8 @@ import soundfile as sf
 import pandas as pd
 import re
 import json
+import glob
+from tqdm import tqdm
 
 try:
     with open('./specmodel/config.json', 'r') as file:
@@ -95,11 +97,11 @@ def convert_waveform_to_mel_spec():
                                                     hop_length=HOP_LENGTH,
                                                     win_length=WIN_LENGTH,
                                                     fmin=FMIN, 
-                                                    fmax=FMAX, 
+                                                    #fmax=FMAX, 
                                                     n_mels=N_MELS, 
                                                     power=POWER)
                 #use log to get to human perceptive loudness levels
-                mel_log = np.log(np.clip(mel, 1e-5, None))
+                mel_log = np.log(np.clip(mel, 1e-8, None))
                 np.save(f"./data/mels/{i}-of-15/{filename}.npy", mel_log.astype(np.float32))
                 counter += 1
                 total_counter += 1
@@ -108,5 +110,40 @@ def convert_waveform_to_mel_spec():
 
     print("Total Number of waveforms converted: ", total_counter)
 
+def normalize_stats():
+    mel_files = glob.glob("./data/mels/**/*.npy", recursive=True)
+
+    sum_ = None
+    sum_sq = None
+    count = 0
+
+    for f in tqdm(mel_files):
+        mel = np.load(f)      # (80, T)
+        mel = mel.T           # (T, 80)
+
+        if sum_ is None:
+            sum_ = np.zeros(mel.shape[1])
+            sum_sq = np.zeros(mel.shape[1])
+
+        sum_ += mel.sum(axis=0)            # sum over time
+        sum_sq += (mel ** 2).sum(axis=0)
+        count += mel.shape[0]
+
+    mean = sum_ / count
+    std = np.sqrt(sum_sq / count - mean**2)
+
+    # safety clamp
+    std = np.where(std < 1e-6, 1e-6, std)
+
+    np.savez("mel_stats.npz", mean=mean, std=std)
+
+    print("Saved mel_stats.npz")
+    print("Mean shape:", mean.shape)
+    print("Std shape:", std.shape)
+    print("Mean range:", mean.min(), mean.max())
+    print("Std range:", std.min(), std.max())
+
+
 #convert_parquet_to_waveform()
 convert_waveform_to_mel_spec()
+normalize_stats() 

specmodel/audio_train.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def train_mel(model_type, model, train_dataset, val_dataset, num_epochs=50, batch_size=4, lr=1e-4, output_dir="../outputs", patience=7, min_delta=1e-4, save_path=None):
+def train_mel(model_type, model, train_dataset, val_dataset, num_epochs=50, batch_size=4, lr=1e-4, output_dir="../outputs", patience=7, min_delta=1e-5, save_path=None):
 
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=False)

specmodel/mel_dataset.py
@@ -33,18 +33,23 @@ class MelMaskedDataset(Dataset):
     def __init__(
         self,
         mel_dir,
+        n_mels,
         mask_seconds=3.0,
         total_clip_length=10.0,
         sr=22050, #sampling rate of spectrogram
         hop_length=256, #number of samples between frames
         max_songs=527, #number of songs in dataset for training and val
-        seed=42
+        seed=42, 
+        normalize=True,
+        stats_path="../mel_stats.npz"
     ):
         self.sr = sr
         self.hop_length = hop_length
         self.mask_seconds = mask_seconds
         self.total_clip_length = total_clip_length
         self.max_songs = max_songs
+        self.n_mels = n_mels
+        self.normalize = normalize
 
         self.files = sorted(glob.glob(os.path.join(mel_dir, "**", "*.npy"), recursive=True))
 
@@ -54,6 +59,12 @@ class MelMaskedDataset(Dataset):
 
         #number of frames to be masked = mask seconds * sampling rate / hop_length
         self.mask_frames = int(mask_seconds * sr / hop_length)
+
+        if self.normalize:
+            stats = np.load(stats_path)
+            self.mel_mean = stats["mean"]   #(80,)
+            self.mel_std  = stats["std"]    #(80,)
+            self.mel_std = np.where(self.mel_std < 1e-6, 1e-6, self.mel_std)
 
     def __len__(self):
         return len(self.files)
@@ -69,8 +80,17 @@ class MelMaskedDataset(Dataset):
             mel = mel[:TARGET_FRAMES]
         elif mel.shape[0] < TARGET_FRAMES:
             pad_len = TARGET_FRAMES - mel.shape[0]
-            pad = np.zeros((pad_len, 80))
+
+            #pad with mean instead of 0 after norm
+            if self.normalize:
+                pad = np.tile(self.mel_mean, (pad_len, 1))
+            else:
+                pad = np.zeros((pad_len, self.n_mels))
+
             mel = np.concatenate([mel, pad], axis=0)
+
+        if self.normalize:
+            mel = (mel - self.mel_mean) / self.mel_std
 
         return torch.from_numpy(mel).float(), filepath  # (T, 80)
     

specmodel/mel_utils.py
@@ -11,6 +11,10 @@ import librosa
 import os
 from datetime import datetime
 
+stats = np.load("../mel_stats.npz")
+mel_mean = stats["mean"]
+mel_std  = stats["std"]
+
 def write_to_waveform(filename, mel_tensor, generator, sr):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -51,6 +55,10 @@ def compare_mels(filepath, model_type, groundtruth, predmel, baseline, sample_ra
     gt_np = groundtruth.detach().cpu().numpy().T
     pred_np = predmel.detach().cpu().numpy().T
     baseline_np = baseline.detach().cpu().numpy().T
+
+    gt_np = gt_np * mel_std[:, None] + mel_mean[:, None]
+    pred_np = pred_np * mel_std[:, None] + mel_mean[:, None]
+    baseline_np = baseline_np * mel_std[:, None] + mel_mean[:, None]
 
     #print("gt_np.shape: ", gt_np.shape)
     #print("pred_np.shape: ", pred_np.shape)
@@ -114,14 +122,20 @@ def compare_mels(filepath, model_type, groundtruth, predmel, baseline, sample_ra
     plt.savefig(f"{output_dir}/mel_debug_{model_type}_{filename}.png", dpi=300, bbox_inches="tight")
     plt.close()
 
-def generate_waveforms(filepath, model_type, groundtruth, predmel, baseline, sample_rate, n_fft, hop_length, n_iter, win_length, fmin, fmax, output_dir="../outputs"):
+def generate_waveforms(filepath, model_type, groundtruth, predmel, baseline, sample_rate, n_fft, hop_length, n_iter, win_length, fmin, fmax, power, output_dir="../outputs"):
     #generate waveforms of predicted and also ground truth with griffin lim for a fair reconstruction comparison
     gt_np = groundtruth.detach().cpu().numpy().T
     pred_np = predmel.detach().cpu().numpy().T
-    baseline_np = predmel.detach().cpu().numpy().T
+    baseline_np = baseline.detach().cpu().numpy().T
 
     #print("gt_np.shape: ", gt_np.shape)
     #print("pred_np.shape: ", pred_np.shape)
+    
+    #denorm and then undo log
+    gt_np = gt_np * mel_std[:, None] + mel_mean[:, None]
+    pred_np = pred_np * mel_std[:, None] + mel_mean[:, None]
+    baseline_np = baseline_np * mel_std[:, None] + mel_mean[:, None]
+
     # Undo natural log
     gt_np = np.exp(gt_np)
     pred_np = np.exp(pred_np)
@@ -129,16 +143,16 @@ def generate_waveforms(filepath, model_type, groundtruth, predmel, baseline, sam
 
     filename = os.path.basename(filepath).rstrip(".npy")
     
-    audio_pred = _get_waveform(pred_np, sample_rate, n_fft, hop_length, n_iter, win_length, fmin, fmax)
-    audio_gt = _get_waveform(gt_np, sample_rate, n_fft, hop_length, n_iter, win_length, fmin, fmax)
-    audio_base = _get_waveform(baseline_np, sample_rate, n_fft, hop_length, n_iter, win_length, fmin, fmax)
+    audio_pred = _get_waveform(pred_np, sample_rate, n_fft, hop_length, n_iter, win_length, fmin, fmax, power)
+    audio_gt = _get_waveform(gt_np, sample_rate, n_fft, hop_length, n_iter, win_length, fmin, fmax, power)
+    audio_base = _get_waveform(baseline_np, sample_rate, n_fft, hop_length, n_iter, win_length, fmin, fmax, power)
 
     sf.write(f"{output_dir}/mel_debug_{model_type}_{filename}_pred.wav", audio_pred, sample_rate)
     sf.write(f"{output_dir}/mel_debug_{model_type}_{filename}_gt.wav", audio_gt, sample_rate)
     sf.write(f"{output_dir}/mel_debug_{model_type}_{filename}_bl.wav", audio_base, sample_rate)
 
 
-def _get_waveform(mel, sample_rate, n_fft, hop_length, n_iter, win_length, fmin, fmax):
+def _get_waveform(mel, sample_rate, n_fft, hop_length, n_iter, win_length, fmin, fmax, power):
     audio = librosa.feature.inverse.mel_to_audio(
         mel,
         sr=sample_rate,
@@ -147,7 +161,8 @@ def _get_waveform(mel, sample_rate, n_fft, hop_length, n_iter, win_length, fmin,
         n_iter=n_iter, #number of iterations for griffin lim
         win_length=win_length, 
         fmin=fmin, 
-        fmax=fmax
+        #fmax=fmax, 
+        power=power
     )
     #ensure amplitudes are in safe range and doesn't blow speakers
     audio /= (np.max(np.abs(audio)) + 1e-9)